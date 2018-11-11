5c2bfa0b8e3df11327c5a84890cb47d42cfa0064
kenan0078 <kenan0078_@hotmail.com>
2018-11-11 10:21:53 +0300

    Data and Variable Types
    write priimitive types as reference types

---
+++ day1/src/day1/work2.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class work2 {
+	public static void main(String[] args)
+	{
+		int i=5; //primitive type variable
+		
+		Integer j=new Integer(6); //reference type variable
+		
+		System.out.println(i);    //print i
+		
+		System.out.println(j);    //print j
+	}
+
+}
